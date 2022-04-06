20d1f8cbe68de15110f249b40debaf73d32a84ed
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-04-06 06:33:15 -0700

    update

--- script.js
+++ script.js
@@ -55,7 +55,82 @@
 // console.log(res);
 
 
-if (true){
-    let name = "sina"
-    console.log(name);
-}
+// if (true){
+//     var name = "sina"
+// }
+// console.log(name);
+
+
+// function func(params) {
+//    let name = "sina"
+// }
+
+// func()
+
+// console.log(name);
+
+
+// const obj = {
+//     name : "sina",
+//     age : 19 ,
+//     family : "Ern"
+// }
+
+// const {name , age , family} = obj
+
+// console.log(name);
+// console.log(age);
+// console.log(family);
+
+
+
+// let [first , secound , third , ...rest] = [1,2,3 ,4,5,6]
+
+// console.log(first);
+// console.log(secound);
+// console.log(third);
+
+// console.log(rest);
+
+
+// let pos = [1,2,3]
+// let neg = [-1,-2,-3]
+
+// console.log([...neg , ...pos]);
+
+// function roll() {
+//     console.log(Math.floor(Math.random() * 7));
+// }
+
+// function higher(func) {
+//     func()
+//     func()
+// }
+
+// higher(roll)
+
+// function func(first = 1 , second = 2) {
+//     console.log(first * second);
+// }
+
+// func(10)
+
+
+// let arr = ["sina",12 , "Ern"]
+
+// let arr2 = [...arr]
+
+// arr2.push("admin")
+
+// console.log(arr2);
+
+
+
+// function medal(first , second , ...others) {
+//     console.log(`Gold ${first}`);
+//     console.log(`Silver ${second}`);
+//     console.log(`thanks the others ${others}`);
+// }
+
+
+// medal("sina","ali","mmd","hasan","jasem","gholi","harchi")
