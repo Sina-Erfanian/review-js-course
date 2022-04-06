d879ff2408ed1057731cf62847379431bc6d4987
Sina-Erfanian <92786381+Sina-Erfanian@users.noreply.github.com>
2022-04-06 04:55:02 -0700

    update

---
+++ script.js
@@ -0,0 +1,61 @@
+// let arr1 = [1,2,3]
+// let arr2 = [1,2,3]
+
+// console.log(arr1 === arr2);
+
+// let name1 = "sina"
+// let name2 = "sina"
+
+// console.log(name1 === name2);
+
+
+// let age = 18
+
+// let newAge = age
+
+// newAge++
+
+// console.log(age);
+// console.log(newAge);
+
+// let person = {
+//     name : "sina",
+//     age : 19
+// }
+
+// let anotherPerson = person
+
+// anotherPerson.family = "Ern"
+
+// console.log(person);
+// console.log(anotherPerson);
+
+
+// let arr = [1,3,4,12]
+
+// const res = arr.reduce((acc,curr)=> {
+//     console.log("acc : " + acc);
+//     console.log("curr : " + curr);
+//     return acc + curr
+// },0)
+
+// console.log(res);
+
+
+// let arr = [
+//     { name : "laptop" , price : 20 },
+//     { name : "tablet" , price : 50 },
+//     { name : "phone" , price : 30 }
+// ]
+
+// let res = arr.reduce((acc,curr)=> {
+//     return acc + curr.price
+// },10)
+
+// console.log(res);
+
+
+if (true){
+    let name = "sina"
+    console.log(name);
+}
